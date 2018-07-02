2441d6c0d44b5d9425e679a22bbcef8c4a824b5c
Lorenzo <Lorenzo>
2018-07-02 10:34:50 +0200

    make it better

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'xEP8pC7SN8AZkQoOQ1X74qRqg',
+  consumer_secret:      '6z7d838zxFRu3d8iQE4CowpC7GoWZSQT6ipiQqTZBoI1hEzRjb',
+  access_token:         '1010437548773081088-U0fq0cJStzasjjcbiK3VZm0LFCAexz',
+  access_token_secret:  'tAIHIQCxIcWqFHdWVwShnbZEdb8snAZXm9f8nJ6g0hwsk'
+}
+
+module.exports = config;
