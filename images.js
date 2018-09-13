137025e85ab9640df73da6454b4caa2a591ebb8a
Lorenzo <Lorenzo>
2018-09-13 17:10:39 +0200

    make it better

--- images.js
+++ images.js
@@ -1496,8 +1496,8 @@ var images = [
     source: 'Credit: しろみそ'
   },
 {
-    file: 'せ''そ_1.png',
-    source: 'Credit: せ''そ_1'
+    file: 'せそ_1.png',
+    source: 'Credit: せ"そ_1'
   },
 {
     file: 'たかたか_1.jpg',
